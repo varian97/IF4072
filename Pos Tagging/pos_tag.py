d6424066508d17b008a47faa32bb72a6d7b2e7ed
Robert Sebastian Herlim <ngiooong@yahoo.com>
2017-09-24 00:44:01 +0700

    Add additional feature to increase accuracy

--- Pos Tagging/pos_tag.py
+++ Pos Tagging/pos_tag.py
@@ -30,11 +30,30 @@ for i in range(0, len(data_parsed)):
         else:
             word_after = data_parsed[i][j+1].get('form')
         
+        # features from http://nlpforhackers.io/training-pos-tagger/
+        is_first = data_parsed[i][j].get('id') == 1
+        is_last = data_parsed[i][j].get('id') == len(data_parsed[i])
+        is_capitalized = form[0].upper() == form[0]
+        prefix_2 = form[:2]
+        prefix_3 = form[:3]
+        suffix_2 = form[-2:]
+        suffix_3 = form[-3:]
+        is_numeric = form.isdigit()
+        
         temp_feature = []
         temp_feature.append(form)
         temp_feature.append(word_before)
         temp_feature.append(word_after)
         temp_feature.append(postag_before)
+        
+        temp_feature.append(is_first)
+        temp_feature.append(is_last)
+        temp_feature.append(is_capitalized)
+        temp_feature.append(prefix_2)
+        temp_feature.append(prefix_3)
+        temp_feature.append(suffix_2)
+        temp_feature.append(suffix_3)
+        temp_feature.append(is_numeric)
         
         feature_data.append(temp_feature)
         target_data.append(data_parsed[i][j].get('upostag'))
@@ -48,7 +67,7 @@ feature_data = np.array(feature_data)
 
 from sklearn import preprocessing
 column_preprocessor = []
-for i in range(0, 4):
+for i in range(0, 12):
     _col_preprocessor = preprocessing.LabelEncoder()
     _col_preprocessor.fit(feature_data[:, i])
     feature_data[:, i] = _col_preprocessor.transform(feature_data[:, i])
@@ -62,7 +81,7 @@ train_data, test_data, train_target, test_target = train_test_split(feature_data
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
-clf = RandomForestClassifier()
+clf = RandomForestClassifier(n_estimators=250)
 clf.fit(train_data, train_target)
 prediction = clf.predict(test_data)
 
