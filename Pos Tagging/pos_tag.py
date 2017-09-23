c6876ff4a8a4a7d1dfbb7f71148ba85f8e8df5c6
Robert Sebastian Herlim <ngiooong@yahoo.com>
2017-09-23 21:07:55 +0700

    Accuracy 0.839083405957 using LabelEncoder

--- Pos Tagging/pos_tag.py
+++ Pos Tagging/pos_tag.py
@@ -1,6 +1,8 @@
 from conllu.parser import parse
+import os
+os.chdir('Desktop\Informatika\Semester_7\IF4072_NLP\IF4072\Pos Tagging')
 
-data = open('id-ud-train.conllu', 'r').read()
+data = open('id-ud-train.conllu', mode='r', encoding='utf-8').read()
 data_parsed = parse(data)
 
 feature_data = []
@@ -40,13 +42,29 @@ for i in range(0, len(data_parsed)):
 print feature_data
 print target_data
 
-'''from sklearn.model_selection import train_test_split
+# PREPROCESSING STRING TO INTEGER (FACTORS)
+import numpy as np
+feature_data = np.array(feature_data)
+
+from sklearn import preprocessing
+column_preprocessor = []
+for i in range(0, 4):
+    _col_preprocessor = preprocessing.LabelEncoder()
+    _col_preprocessor.fit(feature_data[:, i])
+    feature_data[:, i] = _col_preprocessor.transform(feature_data[:, i])
+    column_preprocessor.append(_col_preprocessor)
+
+target_data = column_preprocessor[3].transform(target_data)
+
+# TRAINING MODEL
+from sklearn.model_selection import train_test_split
 train_data, test_data, train_target, test_target = train_test_split(feature_data, target_data, test_size = 0.2)
 
 from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB()
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
 clf.fit(train_data, train_target)
 prediction = clf.predict(test_data)
 
 from sklearn.metrics import accuracy_score
-print 'accuracy = ', accuracy_score(test_target, prediction)'''
+print ('accuracy = ', accuracy_score(test_target, prediction))
